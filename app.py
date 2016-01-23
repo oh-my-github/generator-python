e64925f240f1601a6de27df2e048ca9392cb0cac
NohSeho <iam@nohseho.kr>
2016-01-23 14:07:00 +0900

    feat: Load user's repositories

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from github import Github
+from datetime import datetime
 
 import json
+
+def json_serial(obj):
+  """JSON Serializer for objects not serializable by default json code"""
+  if isinstance(obj, datetime):
+    return obj.isoformat()
+  # raise return obj
+  """TypeError("Type not serializable")"""
 
 AUTH_TOKEN = 'PLEASE TYPE YOUR TOKEN'
 data = json.loads(open('oh-my-github.sample.json', 'r').read())
@@ -33,13 +41,22 @@ omg_data['user']['created_at'] = github_user.created_at
 omg_data['user']['updated_at'] = github_user.updated_at
 omg_data['user']['url'] = github_user.url
 
-from datetime import datetime
 
-def json_serial(obj):
-  """JSON Serializer for objects not serializable by default json code"""
-  
-  if isinstance(obj, datetime):
-    return obj.isoformat()
-  raise TypeError("Type not serializable")
+omg_data['repositories'] = []
+omg_data['languages'] = []
+repos = github_user.get_repos()
+
+for repo in repos:
+    repo_data = dict()
+    repo_data['name'] = repo.name
+    repo_data['full_name'] = repo.full_name
+    repo_data['forks_count'] = repo.forks_count
+    repo_data['stargazers_count'] = repo.forks_count
+    repo_data['watchers_count' ] = repo.watchers_count
+    repo_data['language'] = repo.language
+    repo_data['fork'] = repo.fork is None if False else True
+    repo_data['open_issues_count'] = repo.open_issues_count
+    repo_data['url'] = repo.url
+    omg_data['repositories'].append(repo_data)
 
 print(json.dumps(omg_data, indent=4, default=json_serial))
